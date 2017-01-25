0e5767db02af388dbdbed79bfc52e7f62052ff2f
GarrettHaley <haleyg@oregonstate.edu>
2017-01-25 15:47:09 -0800

    added class Point ii

---
+++ src/main/java/edu/oregonstate/cs361/battleship/Point.java
@@ -0,0 +1,21 @@
+package edu.oregonstate.cs361.battleship;
+
+/**
+ * Created by Garrett on 1/25/2017.
+ */
+public class Point {
+    private int across;
+    private int down;
+}
+public Point(){
+    across = 0;
+    down = 0;
+}
+public Point(Point toCopy){
+   across =  toCopy.across;
+   down = toCopy.down;
+}
+public void display(){
+    System.out.println("Across: " across);
+    System.out.println("Down" down);
+}
